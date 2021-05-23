b0890ebfde8711ae2767ffb6d2f93570dc3e4534
svaldessamperio <svaldes_samperio@hotmail.com>
2021-05-22 21:26:06 -0500

    Tarea 8

--- src/screens/TareaScreen.tsx
+++ src/screens/TareaScreen.tsx
@@ -29,6 +29,7 @@ const styles = StyleSheet.create({
         backgroundColor: '#7b07bf',
     },
     cajaNaranja: {
+        left: 100,
         width: 100,
         height: 100,
         borderWidth: 10,
